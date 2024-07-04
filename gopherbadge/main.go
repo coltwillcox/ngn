e7872286083ad260049cd7d73eda78257a697a6a
Colt Willcox <necromanga@gmail.com>
2024-07-04 19:52:16 +0200

    Add buttons calls

--- gopherbadge/main.go
+++ gopherbadge/main.go
@@ -1,3 +1,4 @@
+// TODO LED on new notification
 package main
 
 import (
@@ -13,54 +14,75 @@ import (
 	"github.com/coltwillcox/ngn/gopherbadge/views"
 )
 
+type Notification struct {
+	Program string
+	Title   string
+	Body    string
+	Sender  string
+	Serial  string
+	// CreatedAt time.Time `json:"created_at"`
+}
+
 const (
-	carriageReturn          = 10 // Character code for a new line.
-	newLine                 = 13 // Character code for a new line.
-	footerX           int16 = 0
-	footerY           int16 = 217
-	currentRectWidth  int16 = 8
-	currentRectHeight int16 = 16
-	currentRectSpace  int16 = 6
-	maximumRects      int   = 10
-	screenWidth       int16 = 320
-	screenHeight      int16 = 240
-	margin            int16 = 8
-	textViewHeight    int16 = 30
+	carriageReturn       = 10 // Character code for a new line.
+	newLine              = 13 // Character code for a new line.
+	footerX        int16 = 0
+	footerY        int16 = 217
+	pageRectWidth  int16 = 8
+	pageRectHeight int16 = 16
+	pageRectSpace  int16 = 6
+	maximumRects   int   = 10
+	screenWidth    int16 = 320
+	screenHeight   int16 = 240
+	margin         int16 = 8
+	textViewHeight int16 = 30
+	historySize          = 10
 )
 
 var (
-	uart = machine.Serial // Serial port stream.
-
-	display = st7789.New(machine.SPI0,
-		machine.TFT_RST,       // TFT_RESET
-		machine.TFT_WRX,       // TFT_DC
-		machine.TFT_CS,        // TFT_CS
-		machine.TFT_BACKLIGHT) // TFT_LITE
-
-	// Main colors in RGBA code.
-	black  = color.RGBA{0, 0, 0, 255}
-	white  = color.RGBA{255, 255, 255, 255}
-	red    = color.RGBA{255, 0, 0, 255}
-	blue   = color.RGBA{0, 0, 255, 255}
-	green  = color.RGBA{0, 255, 0, 255}
-	violet = color.RGBA{116, 58, 213, 255}
-
-	font        = &freemono.Regular9pt7b // Font used to display the text.
-	historySize = 10
-	history     = make([]*Notification, 0, historySize)
-	currentPage = 0
-
+	uart                 = machine.Serial // Serial port stream.
+	display              = st7789.New(machine.SPI0, machine.TFT_RST, machine.TFT_WRX, machine.TFT_CS, machine.TFT_BACKLIGHT)
+	black                = color.RGBA{0, 0, 0, 255}
+	white                = color.RGBA{255, 255, 255, 255}
+	red                  = color.RGBA{255, 0, 0, 255}
+	blue                 = color.RGBA{0, 0, 255, 255}
+	green                = color.RGBA{0, 255, 0, 255}
+	violet               = color.RGBA{116, 58, 213, 255}
+	font                 = &freemono.Regular9pt7b // Font used to display the text.
 	screenBorderRectView = views.RectView{}
 	programTextView      = views.TextView{}
 	senderTextView       = views.TextView{}
 	messageTextView      = views.TextView{}
+	history              = make([]*Notification, 0, historySize)
+	pagesRectViews       = make([]views.RectView, historySize)
+	currentPage          = 0
+	buttonA              = machine.BUTTON_A
+	buttonB              = machine.BUTTON_B
+	buttonUp             = machine.BUTTON_UP
+	buttonLeft           = machine.BUTTON_LEFT
+	buttonDown           = machine.BUTTON_DOWN
+	buttonRight          = machine.BUTTON_RIGHT
 )
 
 func main() {
+	configure()
+	drawUI()
+	drawFooter()
+	loop()
+}
+
+func configure() {
 	machine.SPI0.Configure(machine.SPIConfig{
 		Frequency: 8000000,
 		Mode:      0,
 	})
+
+	buttonA.Configure(machine.PinConfig{Mode: machine.PinInput})
+	buttonB.Configure(machine.PinConfig{Mode: machine.PinInput})
+	buttonUp.Configure(machine.PinConfig{Mode: machine.PinInput})
+	buttonLeft.Configure(machine.PinConfig{Mode: machine.PinInput})
+	buttonDown.Configure(machine.PinConfig{Mode: machine.PinInput})
+	buttonRight.Configure(machine.PinConfig{Mode: machine.PinInput})
 
 	display.Configure(st7789.Config{
 		Rotation: st7789.ROTATION_270,
@@ -69,16 +91,37 @@ func main() {
 	})
 
 	display.FillScreen(black)
-	drawUI()
-	drawFooter()
+	for i := 0; i < len(pagesRectViews); i++ {
+		pagesRectViews[i].SetDisplay(&display).SetDimensions(footerX+margin+(int16(i)*(pageRectWidth+pageRectSpace)), footerY, pageRectWidth, pageRectHeight).SetColor(&violet)
+	}
+}
 
-	// printProgram("123456789012345678901234567890")
-	// printSender("123456789012345678901234567890")
-	// printMessage("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")
+func drawUI() {
+	screenBorderRectView.SetDisplay(&display).SetColor(&violet).SetDimensions(0, 0, screenWidth, screenHeight).Draw()
+	programTextView.SetDisplay(&display).SetFont(font).SetColor(&violet).SetDimensions(margin, margin, screenWidth-margin*2-40, textViewHeight).SetText("111111111111111112222222222222222222222").Draw()
+	senderTextView.SetDisplay(&display).SetFont(font).SetColor(&violet).SetDimensions(margin, textViewHeight+margin*2-1, screenWidth-margin*2, textViewHeight).Draw().SetText("111111111111111112222222222222222222222")
+	messageTextView.SetDisplay(&display).SetFont(font).SetColor(&violet).SetDimensions(margin, textViewHeight*2+margin*3-2, 304, 126).Draw().SetText("1111111111aaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaaaaa11112222222222222222222222w12345678901111111111aaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaaaaa11112222222222222222222222w1234567890")
+}
 
+func drawFooter() {
+	// tinydraw.FilledRectangle(&display, footerX, footerY, screenWidth, screenHeight-footerY, black)
+	for i := 0; i < historySize; i++ {
+		if len(history) > i && history[i] != nil {
+			pagesRectViews[i].SetBackgroundColor(&violet).Draw()
+		} else {
+			pagesRectViews[i].SetBackgroundColor(&black).Draw()
+		}
+	}
+	tinyfont.WriteLine(&display, font, footerX+margin+225, footerY+13, "L/R/A/B", violet)
+}
+
+func loop() {
 	for {
+		// TODO Buttons func
+		checkButtons()
+
 		if uart.Buffered() == 0 {
-			time.Sleep(1000 * time.Millisecond)
+			time.Sleep(50 * time.Millisecond)
 			continue
 		}
 
@@ -124,35 +167,6 @@ func fromMessage(serialMessage []byte) *Notification {
 	}
 
 	return &notification
-}
-
-// TODO Separate fields into eg 2 parts, so they can be wrapped
-type Notification struct {
-	Program string
-	Title   string
-	Body    string
-	Sender  string
-	Serial  string
-	// CreatedAt time.Time `json:"created_at"`
-}
-
-func drawUI() {
-	screenBorderRectView.SetDisplay(&display).SetColor(&violet).SetDimensions(0, 0, screenWidth, screenHeight).Draw()
-	programTextView.SetDisplay(&display).SetFont(font).SetColor(&violet).SetDimensions(margin, margin, screenWidth-margin*2-40, textViewHeight).SetText("111111111111111112222222222222222222222").Draw()
-	senderTextView.SetDisplay(&display).SetFont(font).SetColor(&violet).SetDimensions(margin, textViewHeight+margin*2-1, screenWidth-margin*2, textViewHeight).Draw().SetText("111111111111111112222222222222222222222")
-	messageTextView.SetDisplay(&display).SetFont(font).SetColor(&violet).SetDimensions(margin, textViewHeight*2+margin*3-2, 304, 126).Draw().SetText("1111111111aaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaaaaa11112222222222222222222222w12345678901111111111aaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaa1aaaaaaaaaaaaaaaaaaaaaaaaa11112222222222222222222222w1234567890")
-}
-
-func drawFooter() {
-	// tinydraw.FilledRectangle(&display, footerX, footerY, screenWidth, screenHeight-footerY, black)
-	for i := 0; i < historySize; i++ {
-		if len(history) > i && history[i] != nil {
-			drawFilledRectangle(&display, footerX+margin+(int16(i)*(currentRectWidth+currentRectSpace)), footerY, currentRectWidth, currentRectHeight, violet)
-		} else {
-			drawRectangle(&display, footerX+margin+(int16(i)*(currentRectWidth+currentRectSpace)), footerY, currentRectWidth, currentRectHeight, violet)
-		}
-	}
-	tinyfont.WriteLine(&display, font, footerX+margin+225, footerY+13, "L/R/A/B", violet)
 }
 
 func addToHistory(notification *Notification) {
@@ -203,13 +217,31 @@ func drawCurrentPage() {
 	messageTextView.SetText(currentNotification.Title)
 }
 
-func drawRectangle(displayer *st7789.Device, x int16, y int16, w int16, h int16, color color.RGBA) {
-	displayer.DrawFastHLine(x, x+w-1, y, color)
-	displayer.DrawFastHLine(x, x+w-1, y+h-1, color)
-	displayer.DrawFastVLine(x, y, y+h-1, color)
-	displayer.DrawFastVLine(x+w-1, y, y+h-1, color)
+func checkButtons() {
+	if buttonLeft.Get() {
+		navigatePage(false)
+	}
+	if buttonRight.Get() {
+		navigatePage(true)
+	}
+	if buttonA.Get() {
+
+	}
+	if buttonB.Get() {
+
+	}
 }
 
-func drawFilledRectangle(displayer *st7789.Device, x int16, y int16, w int16, h int16, color color.RGBA) {
-	displayer.FillRectangle(x, y, w, h, color)
+func navigatePage(advance bool) {
+	move := 1
+	if !advance {
+		move = -1
+	}
+	currentPage += move
+	if currentPage < 0 {
+		currentPage = 0
+	} else if currentPage > historySize-1 {
+		currentPage = historySize - 1
+	}
+	// drawCurrentPage()
 }
